182ffc3f74dd21a90f708b48998390ff3eac7af0
Shaun Inman <shaun@panic.com>
2021-04-23 21:40:32 -0400

    fix superclip renderer (fixes Warbirds horizon)
    https://github.com/libretro/libretro-handy/commit/552e619edefa4d135fa4f147fde86e727ea1f083

--- src/handy-libretro/susie.cpp
+++ src/handy-libretro/susie.cpp
@@ -665,8 +665,8 @@ ULONG CSusie::PaintSprites(void)
          int screen_v_start=(SWORD)mVOFF.Word;
          int screen_v_end=(SWORD)mVOFF.Word+SCREEN_HEIGHT;
 
-         int world_h_mid=screen_h_start+0x8000+(SCREEN_WIDTH/2);
-         int world_v_mid=screen_v_start+0x8000+(SCREEN_HEIGHT/2);
+         int world_h_mid=screen_h_start+(SCREEN_WIDTH/2);
+         int world_v_mid=screen_v_start+(SCREEN_HEIGHT/2);
 
          TRACE_SUSIE2("PaintSprites() screen_h_start $%04x screen_h_end $%04x",screen_h_start,screen_h_end);
          TRACE_SUSIE2("PaintSprites() screen_v_start $%04x screen_v_end $%04x",screen_v_start,screen_v_end);
@@ -737,7 +737,7 @@ ULONG CSusie::PaintSprites(void)
             // line, BUT on superclip we draw all the way to the end of any
             // given line checking each pixel is on screen.
 
-            if(superclip & 0)
+            if(superclip)
             {
                // Check on the basis of each quad, we only render the quad
                // IF the screen is in the quad, relative to the centre of
@@ -769,16 +769,16 @@ ULONG CSusie::PaintSprites(void)
                switch(modquad)
                {
                   case 3:
-                     if((sprite_h>=screen_h_start || sprite_h<world_h_mid) && (sprite_v<screen_v_end   || sprite_v>world_v_mid)) render=TRUE;
+                     if((sprite_h>=screen_h_start || sprite_h<=world_h_mid) && (sprite_v<screen_v_end || sprite_v>=world_v_mid)) render=TRUE;
                      break;
                   case 2:
-                     if((sprite_h>=screen_h_start || sprite_h<world_h_mid) && (sprite_v>=screen_v_start || sprite_v<world_v_mid)) render=TRUE;
+                     if((sprite_h>=screen_h_start || sprite_h<=world_h_mid) && (sprite_v>=screen_v_start || sprite_v<=world_v_mid)) render=TRUE;
                      break;
                   case 1:
-                     if((sprite_h<screen_h_end   || sprite_h>world_h_mid) && (sprite_v>=screen_v_start || sprite_v<world_v_mid)) render=TRUE;
+                     if((sprite_h<screen_h_end || sprite_h>=world_h_mid) && (sprite_v>=screen_v_start || sprite_v<=world_v_mid)) render=TRUE;
                      break;
                   default:
-                     if((sprite_h<screen_h_end   || sprite_h>world_h_mid) && (sprite_v<screen_v_end   || sprite_v>world_v_mid)) render=TRUE;
+                     if((sprite_h<screen_h_end || sprite_h>=world_h_mid) && (sprite_v<screen_v_end || sprite_v>=world_v_mid)) render=TRUE;
                      break;
                }
             }
